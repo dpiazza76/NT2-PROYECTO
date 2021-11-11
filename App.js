0d7c5d197c5021cb9ffbe05dadc48904b2fa9a14
Gustavo Alexander Gimenez Ordoñez <Gustav@DESKTOP-4DF9ATR>
2021-11-11 19:43:19 -0300

    Intentando agregar snakeGame.

--- App.js
+++ App.js
@@ -3,13 +3,11 @@ import { StyleSheet, Text, View, Button } from "react-native";
 import DrawerNavigator from "./components/DrawerNavigator";
 import { NavigationContainer } from "@react-navigation/native";
 import "react-native-gesture-handler";
+import Snake from "./components/SnakeGame/Snake";
 
 export default function App() {
-
   return (
-    <NavigationContainer>
-      <DrawerNavigator />
-    </NavigationContainer>
+    <Snake/>
   );
 }
 
