f8d38a1f3a2e41497ca0dd0493d596eb6c387869
Joaquin Soriano <37905853@comunidadort.edu.ar>
2021-11-18 14:31:49 -0300

    me olvide de esto

--- components/DrawerNavigator/index.js
+++ components/DrawerNavigator/index.js
@@ -8,15 +8,10 @@ import Favs from "../../screens/Favs";
 import Ranking from "../../screens/Ranking";
 import SelectGame from "../../screens/SelectGame";
 import Snake from "../SnakeGame/Snake";
-<<<<<<< Updated upstream
-import UserProfile from "../../screens/UsersProfile";
-import LoginForm from "../User/LoginForm";
-=======
 import UserProfile from '../../screens/UsersProfile';
 import GlobalContext, {authData} from '../../components/global/context';
+import LoginForm from '../User/LoginForm'
 
-
->>>>>>> Stashed changes
 export default function DrawerNavigator() {
   const {AuthData, setAuthData} = useContext(GlobalContext);
 
@@ -25,20 +20,6 @@ export default function DrawerNavigator() {
   const isAuthenticated = () => authData._id !== undefined
 
   return (
-<<<<<<< Updated upstream
-    <Drawer.Navigator>
-      <Drawer.Screen name={"Home"} component={Home} />
-      <Drawer.Screen name={"Login"} component={Login} />
-      <Drawer.Screen name={"LoginLocal"} component={LoginForm} />
-      <Drawer.Screen name={"Register"} component={Register} />
-      <Drawer.Screen name={"Form"} component={Form} />
-      <Drawer.Screen name={"Favs ❤"} component={Favs} />
-      <Drawer.Screen name={"SelectGame"} component={SelectGame} />
-      <Drawer.Screen name={"Snake"} component={Snake} />
-      <Drawer.Screen name={"Ranking"} component={Ranking} />
-      <Drawer.Screen name={"UserProfile"} component={UserProfile} />
-    </Drawer.Navigator>
-=======
       
         (isAuthenticated())?
         <Drawer.Navigator>
@@ -55,8 +36,8 @@ export default function DrawerNavigator() {
         <Drawer.Screen name={"Login"} component={Login} />
         <Drawer.Screen name={"Register"} component={Register} />
         <Drawer.Screen name={"Form"} component={Form} />
+        <Drawer.Screen name={"LoginLocal"} component={LoginForm} />
         </Drawer.Navigator>
     
->>>>>>> Stashed changes
   );
 }
