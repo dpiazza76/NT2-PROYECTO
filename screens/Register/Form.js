a05b2c202547fa8af3f3d7b65217d4c49080589c
gonzlem <43175424@comunidadort.edu.ar>
2021-11-17 21:09:51 -0300

    Added register feature

--- screens/Register/Form.js
+++ screens/Register/Form.js
@@ -1,49 +1,56 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, TextInput, Button, Alert } from 'react-native';
-import { useForm, Controller } from 'react-hook-form';
-import Constants from 'expo-constants';
-import axios from 'axios'
+import * as React from "react";
+import { Text, View, StyleSheet, TextInput, Button, Alert } from "react-native";
+import { useForm, Controller } from "react-hook-form";
+import Constants from "expo-constants";
+import axios from "axios";
 import { postUser } from "../../Api";
 
 const SNAKE_GAME_DEFAULT = {
-  "snake":{
+  snake: {
     maxScore: 0,
     isFav: false,
-    timesPlayed:0
-  }
-}
+    timesPlayed: 0,
+  },
+};
 
 export default () => {
-  const { register, setValue, handleSubmit, control, reset, formState: { errors } } = useForm({
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors },
+  } = useForm({
     defaultValues: {
-      fullname: '',
-      email: '',
-      password: '',
-      gamesStatistics: SNAKE_GAME_DEFAULT
-    }
+      fullname: "",
+      email: "",
+      password: "",
+      gamesStatistics: SNAKE_GAME_DEFAULT,
+    },
   });
   const onSubmit = async (user) => {
-    console.log(user);
+    postUser(user);
   };
 
-  const onChange = arg => {
+  const onChange = (arg) => {
     return {
       value: arg.nativeEvent.text,
     };
   };
 
-  console.log('errors', errors);
+  console.log("errors", errors);
 
   return (
     <View style={styles.container}>
       <Text style={styles.label}>Nombre de usuario</Text>
       <Controller
         control={control}
-        render={({field: { onChange, onBlur, value }}) => (
+        render={({ field: { onChange, onBlur, value } }) => (
           <TextInput
             style={styles.input}
             onBlur={onBlur}
-            onChangeText={value => onChange(value)}
+            onChangeText={(value) => onChange(value)}
             value={value}
           />
         )}
@@ -53,25 +60,25 @@ export default () => {
       <Text style={styles.label}>Email</Text>
       <Controller
         control={control}
-        render={({field: { onChange, onBlur, value }}) => (
+        render={({ field: { onChange, onBlur, value } }) => (
           <TextInput
             style={styles.input}
             onBlur={onBlur}
-            onChangeText={value => onChange(value)}
+            onChangeText={(value) => onChange(value)}
             value={value}
           />
         )}
         name="email"
         rules={{ required: true }}
       />
-       <Text style={styles.label}>Password</Text>
+      <Text style={styles.label}>Password</Text>
       <Controller
         control={control}
-        render={({field: { onChange, onBlur, value }}) => (
+        render={({ field: { onChange, onBlur, value } }) => (
           <TextInput
             style={styles.input}
             onBlur={onBlur}
-            onChangeText={value => onChange(value)}
+            onChangeText={(value) => onChange(value)}
             value={value}
           />
         )}
@@ -86,9 +93,9 @@ export default () => {
           title="Reset"
           onPress={() => {
             reset({
-              firstName: 'Bill',
-              lastName: 'Luo'
-            })
+              firstName: "Bill",
+              lastName: "Luo",
+            });
           }}
         />
       </View>
@@ -107,27 +114,27 @@ export default () => {
 
 const styles = StyleSheet.create({
   label: {
-    color: 'white',
+    color: "white",
     margin: 20,
     marginLeft: 0,
   },
   button: {
     marginTop: 40,
-    color: 'white',
+    color: "white",
     height: 40,
-    backgroundColor: '#ec5990',
+    backgroundColor: "#ec5990",
     borderRadius: 4,
   },
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     paddingTop: Constants.statusBarHeight,
     padding: 8,
-    backgroundColor: '#0e101c',
+    backgroundColor: "#0e101c",
   },
   input: {
-    backgroundColor: 'white',
-    borderColor: '#f0fc',
+    backgroundColor: "white",
+    borderColor: "#f0fc",
     height: 40,
     padding: 10,
     borderRadius: 4,
